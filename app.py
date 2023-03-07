b2702a5131f10c46dd5c404a6b1665ca9664f310
ztrend <zoran.pantoulas@gmail.com>
2023-03-07 16:03:39 +0100

    Created API fast.py file and Streamlit app.py file

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 import streamlit as st
+import requests
+"""
+Creating the user interface front end
+"""
+
+st.write('Welcome to my app')
+
+#import requests
+#path -> model
+
+#params
+#response
+#prediction
