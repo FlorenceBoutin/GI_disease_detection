a83cdc186de1ee5d99598e0e6e673a3dafcc4bdb
Emily Ma <emily.ma.1995@gmail.com>
2023-03-08 11:52:39 +0100

    updates

--- ml_logic/cleaning.py
+++ ml_logic/cleaning.py
@@ -1,42 +1,46 @@
+from ml_logic.params import *
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
 from google.cloud import storage
+from google.oauth2 import service_account
 
-def train_val_test_generator():
+def train_val_test_generator(source = SOURCE):
     """
     Generate the train, validation, and test batches.
     """
-    def load_images(folder):
+    def load_images(path):
         """
-        Enter a folder directory with images to load.
+        Enter a path to load images from.
         """
-        datagen = ImageDataGenerator(rescale = 1. / 255)
-        images = datagen.flow_from_directory(folder,
-                                             target_size = (224, 224),
+        datagen = ImageDataGenerator(rescale = IMAGE_RESCALE_RATIO)
+        images = datagen.flow_from_directory(path,
+                                             target_size = (IMAGE_TARGET_WIDTH, IMAGE_TARGET_HEIGHT),
                                              color_mode = "rgb",
-                                             batch_size = 32,
+                                             batch_size = 64,
                                              class_mode = "categorical")
+
         return images
 
-    bucket_name = os.environ.get("BUCKET_NAME")
+    if source == "local":
+        train_directory = os.path.join(RAW_DATA_PATH, "train")
+        val_directory = os.path.join(RAW_DATA_PATH, "val")
+        test_directory = os.path.join(RAW_DATA_PATH, "test")
 
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
+    #this doesn't work right now
+    if source == "cloud":
+        credentials = service_account.Credentials.from_service_account_file(GOOGLE_APPLICATION_CREDENTIALS)
+        client = storage.Client(project = GCLOUD_PROJECT_ID, credentials = credentials)
+        bucket = client.get_bucket(BUCKET_NAME)
 
-    train_directory = bucket.blob("train/*")
-    val_directory = bucket.blob("val/*")
-    test_directory = bucket.blob("test/*")
-
-    # train_directory = f"{bucket}/train"
-    # val_directory = f"{bucket}/val"
-    # test_directory = f"{bucket}/test"
+        train_directory = f"gs://{BUCKET_NAME}/train"
+        val_directory = f"gs://{BUCKET_NAME}/val"
+        test_directory = f"gs://{BUCKET_NAME}/test"
 
     train_dataset = load_images(train_directory)
     val_dataset = load_images(val_directory)
     test_dataset = load_images(test_directory)
 
-    print(train_dataset, val_dataset, test_dataset)
     return train_dataset, val_dataset, test_dataset
 
 if __name__ == "__main__":
-    print(train_val_test_generator())
+    train_val_test_generator()
